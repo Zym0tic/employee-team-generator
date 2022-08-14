2933765552b7086ceb9560e5b1017f90604792df
zym0tic <lukerozon@gmail.com>
2022-08-14 13:07:01 -0400

    created questions variables

---
+++ index.js
@@ -0,0 +1,94 @@
+const Inquirer = require 
+
+const managerQuestions = [
+    {
+        type: 'input',
+        message: 'Managers name?',
+        name: 'managerName',
+    },
+    {
+        type: 'input',
+        message: 'Managers id?',
+        name: 'managerId',
+    },
+    {
+        type: 'input',
+        message: 'Managers email address?',
+        name: 'managerEmail',
+    },
+    {
+        type: 'input',
+        message: 'Managers office number?',
+        name: 'managerOfficeNum',
+    },
+    {
+        type: 'confirm',
+        message: 'Would you like to add another team mate?',
+        name: 'addTeammate',
+    },
+];
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: 'Engineers name?',
+        name: 'engineerName',
+    },
+    {
+        type: 'input',
+        message: 'Engineers id?',
+        name: 'engineerId',
+    },
+    {
+        type: 'input',
+        message: 'Engineers email address?',
+        name: 'engineerEmail',
+    },
+    {
+        type: 'input',
+        message: 'Engineers Github username?',
+        name: 'engineerGithhub',
+    },
+    {
+        type: 'confirm',
+        message: 'Would you like to add another team mate?',
+        name: 'addTeammate',
+    },
+];
+const internQuestions = [
+    {
+        type: 'input',
+        message: 'Interns name?',
+        name: 'internName',
+    },
+    {
+        type: 'input',
+        message: 'Interns id?',
+        name: 'internId',
+    },
+    {
+        type: 'input',
+        message: 'Interns email address?',
+        name: 'internEmail',
+    },
+    {
+        type: 'input',
+        message: 'Interns school name?',
+        name: 'engineerGithhub',
+    },
+    {
+        type: 'confirm',
+        message: 'Would you like to add another team mate?',
+        name: 'addTeammate',
+    },
+];
+const employeeType = [
+    {
+        type: 'list',
+        message: 'what is the team mates position?',
+        name: 'employeeType',
+        choices: ['engineer', 'intern'],
+    }
+];
+
+Inquirer.prompt(managerQuestions, 
+    )
